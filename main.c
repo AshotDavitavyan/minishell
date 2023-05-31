543d173728503bf617e45054677da19266a8683b
Ashot Davitavyan <adavitav@c2r13s4.42yerevan.am>
2023-05-31 14:42:20 +0400

    test

--- main.c
+++ main.c
@@ -1,5 +1,31 @@
 #include "minishell.h"
 
+char	*get_quote_token(char *user_input, char type)
+{
+	int i;
+
+	i = 1;
+	user_input++;
+	while (*user_input != type)
+	{
+
+	}
+	return (NULL);
+}
+
+void	get_tokens(char *user_input)
+{
+	int i;
+
+	i = 0;
+	while (*user_input)
+	{
+		if (*user_input == 34 || *user_input == 39)
+			user_input = get_quote_token(user_input, *user_input)
+		user_input++;
+	}
+}
+
 static char	*alloc(char *user_input, char *input_new, int j, int i)
 {
 	while (user_input[i])
@@ -60,7 +86,7 @@ char	*put_spaces(char *user_input)
 void	lexing(char *user_input)
 {
 	user_input = put_spaces(user_input);
-	
+	get_tokens(user_input);
 }
 
 int	main(int argc, char **argv, char **env)
