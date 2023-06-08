3bb218c2c6c2dfbc2cd419b781f9e9174bd63780
Ashot Davitavyan <adavitav@c2r14s2.42yerevan.am>
2023-06-08 17:37:47 +0400

    upd

--- mshell_src/minishell.h
+++ mshell_src/minishell.h
@@ -9,13 +9,13 @@
 #include "libft/libft.h"
 #include "ft_printf/ft_printf.h"
 
-// typedef struct s_token
-// {
-// 	t_token	*before;
-// 	int		type;
-// 	char 	*flags;
-// 	char 	*name;
-// 	t_token	*next;
-// }	t_token;
+typedef struct s_token
+{
+	struct t_token	*before;
+	int		type;
+	char 	*flags;
+	char 	*name;
+	struct t_token	*next;
+}	t_token;
 
 #endif
