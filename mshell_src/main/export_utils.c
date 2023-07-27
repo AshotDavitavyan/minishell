5507fde42cebb170358b2048828df3b2217a6e3f
vlad1m1r1337 <95868255+vlad1m1r1337@users.noreply.github.com>
2023-07-27 16:02:28 +0400

    no leak

--- mshell_src/main/export_utils.c
+++ mshell_src/main/export_utils.c
@@ -6,7 +6,7 @@
 /*   By: vgribkov <vgribkov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 12:50:06 by vgribkov          #+#    #+#             */
-/*   Updated: 2023/07/26 16:05:43 by vgribkov         ###   ########.fr       */
+/*   Updated: 2023/07/27 15:44:43 by vgribkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	push_in_arr(t_shell *shell, char *str)
 	j = 0;
 	while (j < i)
 	{
-		new_arr[j] = shell -> envex[j];
+		new_arr[j] = ft_strdup(shell -> envex[j]);
 		j++;
 	}
 	new_arr[j] = ft_strdup(str);
@@ -56,7 +56,10 @@ int	find_dup(t_shell *shell, char *str)
 	while (shell -> envex[i])
 	{
 		if (ft_strncmp(shell -> envex[i], str, ft_strlen(temp)) == 0)
+		{
+			free(temp);
 			return (i);
+		}
 		i++;
 	}
 	free(temp);
@@ -89,6 +92,30 @@ char	*strdup_bez_pls(char *arr)
 	return (new_arr);
 }
 
+char	*ft_strjoin_my(char const *s1, char const *s2)
+{
+	char	*arr;
+	int		i;
+	int		j;
+	int		step;
+
+	if (!s1 || !s2)
+		return (NULL);
+	step = 0;
+	arr = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	i = 0;
+	j = 0;
+	if (!arr)
+		return (NULL);
+	while (s1[i])
+		arr[step++] = s1[i++];
+	while (s2[j])
+		arr[step++] = s2[j++];
+	arr[step] = '\0';
+	free((char *)s2);
+	return (arr);
+}
+
 void	help_exp(t_shell **shell, int i, char *arr)
 {
 	free((*shell)->envex[i]);
@@ -107,22 +134,30 @@ int	bi_export2(t_shell *shell, char **arr)
 	{
 		if ((find_dup(shell, arr[j])) != -1)
 		{
-			i = find_dup(shell, arr[j]);
+			i = find_dup(shell, arr[j]);	
 			if (ft_strchr(arr[j], '+'))
 			{
+				if (!ft_strchr(shell -> envex[i], '='))
+					shell -> envex[i] = ft_strjoin(shell -> envex[i], "=");
 				char *temp = ft_strchr(arr[j], '+') + 2;
 				shell -> envex[i] = ft_strjoin(shell -> envex[i], temp);
 			}
 			else
-				help_exp(&shell, i, arr[j]);
-			return (0);
+			{
+				free((shell)->envex[i]);
+				(shell)->envex[i] = NULL;
+				(shell)->envex[i] = ft_strdup(arr[j]);
+				//help_exp(&shell, i, arr[j]);
+			}
+			//return (0);
 		}
 		else if (ft_strchr(arr[j], '+'))
 		{
 			arr[j] = strdup_bez_pls(arr[j]);
 		}
-		return (push_in_arr(shell, arr[j]));
+		else
+			push_in_arr(shell, arr[j]);
 	}
-	return (1);
+	return (0);
 }
 	
